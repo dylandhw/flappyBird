d37f4c436fd90fa41777ea6b895788e0fad04381
dylan <dylanwilkins2308@gmail.com>
2024-09-17 18:12:52 -0400

    passed in graphics
    set the window size

--- main.java
+++ main.java
@@ -4,7 +4,17 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class main extends JPanel implements ActionListener, KeyListener {}
+public class main extends JPanel implements ActionListener, KeyListener {
+    // sets the size of the window
+    int winWidth = 500;
+    int winHeight = 500;
+
+    // takes in images 
+    Image flappybird;
+    Image flappybirdbg;
+    Image toppipe;
+    Image bottompipe;
+}
 
 
 
